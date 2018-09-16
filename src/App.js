231e061b8723f79a33af75842716080b2060108a
lauren wright <wright.lauren20@gmail.com>
2018-09-16 16:32:20 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      people: [
+        {name: "Jim", alive: true},
+        {name: "Bob", alive: false},
+        {name: "Billy", alive: true}
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <h1> Hello World </h1>
-        <p> This is my cool hello world project. </p>
-        <p className="red"> This text is RED </p>
+        <ul>
+          {this.state.people.map(person =>( 
+            <li>{person.name}</li>
+          ))}
+        </ul>
       </div>
     );
   }
